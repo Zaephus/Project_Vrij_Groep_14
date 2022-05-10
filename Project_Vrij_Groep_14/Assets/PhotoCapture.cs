0dd3638bdccc17216664387e11a6aecba64a3b85
Emmie den Hartog <emmie.denhartog@student.hku.nl>
2022-05-11 01:00:02 +0200

    Camera Prototype

---
+++ Project_Vrij_Groep_14/Assets/PhotoCapture.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotoCapture : MonoBehaviour
+{
+    [Header("Photo Taker")]
+    [SerializeField] Image photoDisplayArea;
+    [SerializeField] GameObject photoFrame;
+
+    Texture2D screenCapture;
+    bool viewingPhoto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!viewingPhoto)
+            {
+                StartCoroutine(CapturePhoto());
+            }
+            else
+            {
+                RemovePhoto();
+            }
+        }
+    }
+
+
+    //enum en coroutine zodat het zeker is dat alle items in beeld zijn geladen voordat de foto wordt genomen
+    IEnumerator CapturePhoto()
+    {
+        viewingPhoto = true;
+        yield return new WaitForEndOfFrame();
+
+        Rect regionToRead = new Rect(0, 0, Screen.width, Screen.height);
+
+        screenCapture.ReadPixels(regionToRead, 0, 0, false);
+        screenCapture.Apply();
+        ShowPhoto();
+    }
+
+    void ShowPhoto()
+    {
+        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0, 0, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100);
+        photoDisplayArea.sprite = photoSprite;
+
+        photoFrame.SetActive(true);
+    }
+
+    void RemovePhoto()
+    {
+        viewingPhoto = false;
+        photoFrame.SetActive(false);
+    }
+
+}
