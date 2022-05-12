83719b2402257afa518654f03ff3bb6e27243dfa
Zaephus <rozendaalmarc@gmail.com>
2022-05-12 15:02:39 +0200

    Player Tweaks

---
+++ Project_Vrij_Groep_14/Assets/1_Scripts/PlayerInteract.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour {
+
+    public void OnStart() {
+
+    }
+
+    public void OnUpdate() {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other) {}
+
+    public void OnTriggerStay2D(Collider2D other) {}
+
+    public void OnTriggerExit(Collider2D other) {}
+
+}
