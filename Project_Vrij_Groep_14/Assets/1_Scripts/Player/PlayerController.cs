1a44caf4abc8cb976139b26c056519b20cbee914
Emmie den Hartog <emmie.denhartog@student.hku.nl>
2022-05-23 10:59:31 +0200

    Player Prefab

--- Project_Vrij_Groep_14/Assets/1_Scripts/Player/PlayerController.cs
+++ Project_Vrij_Groep_14/Assets/1_Scripts/Player/PlayerController.cs
@@ -91,9 +91,9 @@ public class PlayerController : MonoBehaviour {
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation,-20,45);
 
-        playerCamera.localRotation = Quaternion.Euler(xRotation,0,0);
+        //playerCamera.localRotation = Quaternion.Euler(xRotation,0,0);
         //The head is on a different axis
-        playerHead.localRotation = Quaternion.Euler(0,xRotation,0);
+        playerHead.localRotation = Quaternion.Euler(xRotation,0,0);
         playerBody.Rotate(Vector3.up * mouseX);
 
     }
