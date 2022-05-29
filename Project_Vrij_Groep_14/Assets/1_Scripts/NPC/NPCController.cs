5e694d5981f9cdae72b7cd06c39f2562d4cb5be6
Zaephus <rozendaalmarc@gmail.com>
2022-05-29 17:52:43 +0200

    Added NPC walking animations
    Added animations + rewrote the npc controller

--- Project_Vrij_Groep_14/Assets/1_Scripts/NPC/NPCController.cs
+++ Project_Vrij_Groep_14/Assets/1_Scripts/NPC/NPCController.cs
@@ -4,14 +4,53 @@ using UnityEngine;
 
 public class NPCController : MonoBehaviour {
 
-    private FSM fsm;
+    public Animator animator;
+
+    public float walkSpeed = 2f;
+    public float rotateSpeed = 2f;
+
+    public List<Transform> waypoints = new List<Transform>();
+    private int currentWaypointIndex = -1;
+    private Transform targetWaypoint;
+
+    public float stoppingDistance;
 
     public void OnStart() {
-        fsm = new FSM(typeof(IdleState),GetComponents<BaseState>());
+
+        targetWaypoint = GetTargetWaypoint();
     }
 
     public void OnUpdate() {
-        fsm.OnUpdate();
+
+        transform.position = Vector3.MoveTowards(transform.position,targetWaypoint.position,walkSpeed*Time.deltaTime);
+        RotateTowardsTarget();
+
+        animator.SetFloat("Forward",1);
+
+        if(Vector3.Distance(transform.position,targetWaypoint.position) <= stoppingDistance) {
+            targetWaypoint = GetTargetWaypoint();
+        }
     }
-    
+
+    public Transform GetTargetWaypoint() {
+
+        currentWaypointIndex++;
+
+        if(currentWaypointIndex >= waypoints.Count) {
+            currentWaypointIndex = 0;
+        }
+
+        return waypoints[currentWaypointIndex];
+
+    }
+
+    public void RotateTowardsTarget() {
+
+        Vector3 lookPos = targetWaypoint.position - transform.position;
+        lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation,rotateSpeed*Time.deltaTime);
+
+    }
+
 }
